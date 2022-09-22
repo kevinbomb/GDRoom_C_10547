74aa24d50e33e4bf2979b7734950f6c057e603bb
kevinbomb <kevinwomb@gmail.com>
2022-09-22 15:27:57 +0700

    GDRoom_C_10547

--- app/src/main/java/com/example/gdroom_c_10547/room/NoteDB.kt
+++ app/src/main/java/com/example/gdroom_c_10547/room/NoteDB.kt
@@ -4,7 +4,7 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
-
+//comment
 @Database(
     entities = [Note::class],
     version = 1
